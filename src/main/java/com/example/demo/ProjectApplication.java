15094d7b99af3fda230675a865da7c77d866badb
Tran Han <joyhan283@gmail.com>
2020-09-22 15:29:31 +0700

    update source code

--- src/main/java/com/example/demo/ProjectApplication.java
+++ src/main/java/com/example/demo/ProjectApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class ProjectApplication {
+@RestController
 
+public class DemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello";
 	}
 
 }
