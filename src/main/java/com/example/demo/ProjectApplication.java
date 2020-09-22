a8ea9da657707279b5fd937146e23f871570b76e
Tran Han <joyhan283@gmail.com>
2020-09-22 15:51:07 +0700

    update source code

--- src/main/java/com/example/demo/ProjectApplication.java
+++ src/main/java/com/example/demo/ProjectApplication.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 
-public class DemoApplication {
+public class ProjectApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ProjectApplication.class, args);
 	}
 
 	@GetMapping("/")
